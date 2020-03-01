a0c8b2bdfb50d1f7cfc79ca285d03f55b7bc9b6d
Nadia <nadia.krok.adamska@gmail.com>
2020-03-01 12:27:59 +0100

    Commit

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+    static Logger log = Logger.getLogger("name");
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        log.info("message");
+
     }
 }
